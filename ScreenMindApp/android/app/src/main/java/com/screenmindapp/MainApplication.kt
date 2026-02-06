3aabf1afb2ef0775c5b4cb495e6b0e7a7696e4bb
K-G-C-N-Kulathunga <161619772+K-G-C-N-Kulathunga@users.noreply.github.com>
2026-02-06 14:18:59 +0530

    Revert "Merge pull request #7 from K-G-C-N-Kulathunga/dimaasha_main"
    This reverts commit 844487840df15579ad392a0131134cb9486171b9, reversing
    changes made to fa0b16fad35aae7d9ca5de8052f055f0eb42ab56.

--- ScreenMindApp/android/app/src/main/java/com/screenmindapp/MainApplication.kt
+++ ScreenMindApp/android/app/src/main/java/com/screenmindapp/MainApplication.kt
@@ -4,15 +4,8 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-
-// ✅ Manual packages
-import com.screenmindapp.isolation.UsageStatsPackage
-import com.screenmindapp.isolation.ServiceStarterPackage
-import com.screenmindapp.isolation.IsolationMetricsPackage
-import com.screenmindapp.isolation.BehaviourMetricsPackage   // ✅ NEW
-import com.screenmindapp.isolation.CommunicationStatsPackage
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
 class MainApplication : Application(), ReactApplication {
 
@@ -21,15 +14,7 @@ class MainApplication : Application(), ReactApplication {
       context = applicationContext,
       packageList =
         PackageList(this).packages.apply {
-
-          // ✅ Add your manual packages here
-          add(UsageStatsPackage())
-          add(ServiceStarterPackage())
-          add(IsolationMetricsPackage())
-          add(BehaviourMetricsPackage()) // ✅ NEW
-          add(CommunicationStatsPackage())
-
-          // Packages that cannot be autolinked yet can be added manually here
+          // Packages that cannot be autolinked yet can be added manually here, for example:
           // add(MyReactNativePackage())
         },
     )
